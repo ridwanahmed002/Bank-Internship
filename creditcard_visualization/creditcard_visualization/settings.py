0caa6bd6326353eb3156b16ffadb3dff67efd03a
ridwanahmed002 <ridwanahmed.44504@gmail.com>
2024-12-15 12:24:09 +0600

    checked html working

--- creditcard_visualization/creditcard_visualization/settings.py
+++ creditcard_visualization/creditcard_visualization/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "creditcard_visualization.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
